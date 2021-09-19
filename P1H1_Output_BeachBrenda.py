18fbb164ca3dce18349f10e3f8fb1a874052c877
beachb7704 <90799109+beachb7704@users.noreply.github.com>
2021-09-19 16:20:13 -0400

    Add files via upload

---
+++ P1H1_Output_BeachBrenda.py
@@ -0,0 +1,15 @@
+user_num = int(input('Enter integer:\n'))
+
+print("You entered:", user_num)
+
+print(user_num, 'squared is', user_num * user_num)
+
+print("And", user_num, "cubed is", user_num*user_num*user_num, "!!")
+
+user_num2 = int(input('Enter another integer:\n'))
+
+num3 = user_num + user_num2
+print (user_num, '+', user_num2, 'is', num3)
+num4 = user_num * user_num2
+print(user_num, '*', user_num2, 'is', num4)
+
