d6bb56e9181ecdf0b1f2a652fcc7c2c8fe632a67
beachb7704 <90799109+beachb7704@users.noreply.github.com>
2021-09-30 20:30:07 -0400

    Add files via upload

---
+++ P2LAB2_Caffeine_BeachBrenda.py
@@ -0,0 +1,14 @@
+caffeine_mg = float(input())
+
+caf1 = 0.5 * caffeine_mg
+caf2 = 0.5 * caf1
+caf3 = 0.5 * caf2
+caf4 = 0.5 * caf3
+
+
+
+print('After 6 hours: {:.2f} mg'.format(caf1))
+
+print('After 12 hours: {:.2f} mg'.format(caf2))
+
+print('After 24 hours: {:.2f} mg'.format(caf4))
